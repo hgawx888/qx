60f118545aa56a1bdd87b9a4aaca569578bee39c
hgawx888 <131592112+hgawx888@users.noreply.github.com>
2023-04-24 22:59:29 +0800

    Update cpdq.js

--- cpdq.js
+++ cpdq.js
@@ -1,21 +1,16 @@
 /******************************
 
-脚本功能：菜谱大全+解锁VIP
-下载地址：https://is.gd/9ryiqX
+脚本功能：菜谱大全ViP
 软件版本：5.2.3
-脚本作者：彭于晏💞
-更新时间：2022-10-14
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
+脚本作者：hgawx
+更新时间：2023-4-24
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/apiios_dq\.xiangha\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cpdq.js
+^https:\/\/apiios_dq\.xiangha\.com url script-response-body https://raw.githubusercontent.com/hgawx888/qx/main/cpdq.js
 
 [mitm] 
 
