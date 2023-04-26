514266c372eb589b9b88295b6902e14c3ea5fc03
hgawx888 <131592112+hgawx888@users.noreply.github.com>
2023-04-26 23:37:57 +0800

    Update bfcpdq.js

--- bfcpdq.js
+++ bfcpdq.js
@@ -8,7 +8,7 @@
 ***************************
 %¥
 [rewrite_local]
-^https?:\/\/apiios_dq\.xiangha\.com\/v4\/user\/getUserData|https?:\/\/apiios_dq\.xiangha\.com\/v4\/dish\/info.*? url script-response-body https://raw.githubusercontent.com/hgawx888/qx/main/bjcpdq.js
+^https?:\/\/apiios_dq\.xiangha\.com\/v4\/user\/getUserData|https?:\/\/apiios_dq\.xiangha\.com\/v4\/dish\/info.*? url script-response-body https://raw.githubusercontent.com/hgawx888/qx/main/bfcpdq.js
 
 
 [mitm]
