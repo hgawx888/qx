ed0075802ea5e33a205d47e60f934a7ae9fd5be2
hgawx888 <131592112+hgawx888@users.noreply.github.com>
2023-04-26 23:41:30 +0800

    Update bfcpdq.js

--- bfcpdq.js
+++ bfcpdq.js
@@ -1,19 +1,20 @@
-/******************************
-⚠️如果放远程，请把bfcpq.js替换成运程链接⚠️
-  🧚🏻‍♂️作者：🍡魔法师、木木🍡
-  wx交流群：1077223830
-🫧脚本名称:菜谱大全vip
-🫧建议配合working copy一起食用
-✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
-***************************
-%¥
+/*******************************
+
+脚本功能：菜谱大全ViP
+软件版本：5.4.2
+脚本作者：hgawx
+更新时间：2023-4-26
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+******************************** 
+
 [rewrite_local]
 ^https?:\/\/apiios_dq\.xiangha\.com\/v4\/user\/getUserData|https?:\/\/apiios_dq\.xiangha\.com\/v4\/dish\/info.*? url script-response-body https://raw.githubusercontent.com/hgawx888/qx/main/bfcpdq.js
 
 
 [mitm]
-hostname = apiios_dq.xiangha.com,apiios_dq.xiangha.com
-%¥
+hostname = apiios_dq.xiangha.com
+
 *******************************/
 var body=$response.body;
 body = body.replace(/"is_vip\":\d/g,'"is_vip":2');
