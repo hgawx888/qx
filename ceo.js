3e40774b20688ed819719d559366e372f1cb4184
hgawx888 <pyktv2015@163.com>
2023-09-05 23:37:11 +0800

    更新 ceo.js

--- ceo.js
+++ ceo.js
@@ -22,45 +22,17 @@ hostname = user.xiaozaoapp.com
 
 *******************************/
 
-var body = $response.body;
-var objk = JSON.parse(body);
-
-objk = {
-  "errcode" : 200,
-  "data" : {
-    "is_hide" : 0,
-    "profile" : "",
-    "company" : "",
-    "is_set_password" : 0,
-    "nickname" : "彭于晏解锁",
-    "is_reg" : 0,
-    "vip_end_time" : "2088-09-11 19:36:44",
-    "is_cancel" : 0,
-    "channel_name" : "",
-    "user_secret" : "2504900.b414c5a2i9RpTNd536b80dc58ae64185449a90",
-    "rc_token" : "Aub3udkdrTVW9TAZpT7DQHC1W+w8uIw/aT4tzWQQsC0=@ro3r.cn.rongnav.com;ro3r.cn.rongcfg.com",
-    "job" : "",
-    "vip_start_time" : "2088-09-11 19:36:44",
-    "is_vip" : 1,
-    "token" : "TK_J8it27_2504900",
-    "uid" : "2504900",
-    "email" : "",
-    "mobile" : "17876273604",
-    "rand_password" : "kpIvYLvt6SBmLVto",
-    "avatar" : "avatar/11.png",
-    "avatar_large" : "avatar/11.png",
-    "usercode" : "2514900",
-    "is_wechat_binded" : 0,
-    "username" : "17876273604"
-  },
-  "msg" : "操作成功",
-  "ret_code" : 200,
-  "current_time" : "2023-09-05 23:05:18"
+var body = $response['body'];
+var url = $request['url'];
+var obj = JSON['parse'](body);
+const vip = '/loginAuto';
+if (url['indexOf'](vip) != -0x1) {
+    obj['data']['is_vip'] = 0x1;
+    obj['data']['vip_end_time'] = '2088-09-11 19:36:44';
+    obj['data']['vip_start_time'] = '2088-09-11 19:36:44';
+    obj['data']['nickname'] = '彭于晏解锁';
+    body = JSON['stringify'](obj);
 }
-
-
-body = JSON.stringify(objk);
-
 $done({
-    body
+    'body': body
 });
