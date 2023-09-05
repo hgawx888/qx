100baccc662d6595f09251d7f00d4530a98fca95
hgawx888 <pyktv2015@163.com>
2023-09-05 23:41:49 +0800

    更新 ceo.js

--- ceo.js
+++ ceo.js
@@ -2,12 +2,10 @@
 
 脚本功能：CEO周课+解锁VIP
 下载地址：http://mtw.so/6rDbGg
+脚本功能：菜谱大全ViP
 软件版本：1.7.2
-脚本作者：彭于晏
-更新时间：2022-9-11
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
+脚本作者：hgawx
+更新时间：2023-04-26
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
@@ -19,7 +17,6 @@ TG反馈群：https://t.me/plus8889
 [mitm] 
 hostname = user.xiaozaoapp.com
 
-
 *******************************/
 
 var body = $response['body'];
@@ -28,8 +25,8 @@ var obj = JSON['parse'](body);
 const vip = '/loginAuto';
 if (url['indexOf'](vip) != -0x1) {
     obj['data']['is_vip'] = 0x1;
-    obj['data']['vip_end_time'] = '2088-09-11 19:36:44';
-    obj['data']['vip_start_time'] = '2088-09-11 19:36:44';
+    obj['data']['vip_end_time'] = '2099-09-09 08:08:00';
+    obj['data']['vip_start_time'] = '2099-09-09 08:08:00';
     obj['data']['nickname'] = '彭于晏解锁';
     body = JSON['stringify'](obj);
 }
